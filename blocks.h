26f0bc1c6ff4f26419525e54e9e7258212d44702
dosisod <39638017+dosisod@users.noreply.github.com>
2024-01-21 23:03:57 -0800

    Add optional battery status indicator:
    I recently got a new laptop so I added a battery indicator to my status bar.

--- blocks.h
+++ blocks.h
@@ -7,6 +7,9 @@ static const Block blocks[] = {
 	{"", "cat /tmp/current_song 2>/dev/null | sed 's/....$//'", 1, 0},
 	{"", "[ -f /tmp/current_playlist ] && basename \"$(cat /tmp/current_playlist)\" | sed -e 's/|//g' -e 's/^/ /'", 1, 0},
 	{"", "date '+%a %b %-d %H:%M'", 5, 0},
+#ifdef LAPTOP
+	{"", "echo \"$(cat /sys/class/power_supply/BAT1/capacity)%$([ \"$(cat /sys/class/power_supply/BAT1/status)\" = \"Discharging\" ] && echo \" !\")\"", 3, 0},
+#endif
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
